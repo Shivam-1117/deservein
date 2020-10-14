f5bdac6d2ce8a268d3766a272d8e682e64f36bc8
Shivam Mishra <55855396+Shivam-1117@users.noreply.github.com>
2020-10-14 11:27:58 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,4 +1,11 @@
 <?php
 $pdo = "dbname=d9ttbplbaq2kmf host=ec2-54-152-40-168.compute-1.amazonaws.com port=5432 user=lqvlqvphacsifs password=f7370e21bd526162cc3dd18c723119040aaeb3a1bce00ccd4da687a8abe90980 sslmode=require";
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if (!$pdo) 
+   {
+     echo "Database connection failed.";
+   }
+   else 
+   {
+     echo "Database connection success.";
+   }
 ?>
